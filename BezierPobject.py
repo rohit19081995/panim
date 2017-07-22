1d536c04168ef52ad7eb98ce6ec1e1abb503273e
Rohit <rohit19081995@gmail.com>
2017-07-22 12:52:55 +0530

    Added smoothing for any set of points, but not yet robust for usage

--- BezierPobject.py
+++ BezierPobject.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-points = [[0,0],
-		  [1,1],
-		  [2,0],
-		  [3,0]]
+points = [[1,0],
+		[2,1],
+		[3,0],
+		[4,0],]
 # def get_handle_points(points, closed=False):
+handle_points = []
 closed = False
 n = len(points)-1
 for index in range(len(points[0])):
@@ -15,8 +16,8 @@ for index in range(len(points[0])):
 		matrix[2*i-1,2*i] = 1
 		matrix[2*i-1,2*(i-1)+1] = 1
 		rhs[2*i-1] = 2*points[i][index]
-		matrix[2*i, 2*i] = -2
-		matrix[2*i, 2*i+1] = 1
+		matrix[2*i, 2*i] = 2
+		matrix[2*i, 2*i+1] = -1
 		matrix[2*i, 2*(i-1)] = 1
 		matrix[2*i, 2*(i-1)+1] = -2
 
@@ -32,10 +33,17 @@ for index in range(len(points[0])):
 	else:
 		matrix[0, 0] = -2
 		matrix[0, 1] = 1
-		rhs[0] = points[0][index]
+		rhs[0] = -points[0][index]
 		matrix[2*n-1, 2*n-1] = -2
 		matrix[2*n-1, 2*n-2] = 1
-		rhs[2*n-1] = points[n][index]
-	x = np.linalg.solve(matrix, rhs)
-	print(np.allclose(np.dot(matrix,x), rhs))
-	print(x)
+		rhs[2*n-1] = -points[n][index]
+	print(matrix)
+	print(rhs)
+	handle_points.append(np.linalg.solve(matrix, rhs))
+
+handle_points = np.array(handle_points).swapaxes(0,1)
+p1arr = handle_points[::2]
+p2arr = handle_points[1::2]
+svg_string = 'M %f,%f' % (points[0][0], points[0][1])
+for i in range(n):
+	svg_string += 'C %f,%f %f,%f %f,%f' % (p1arr[i][0], p1arr[i][1], p2arr[i][0], p2arr[i][1], points[i+1][0], points[i+1][1])
--- common.py
+++ common.py
@@ -1,5 +1,10 @@
+from constants import *
+
 class PanimException(Exception):
 	'''
 	An exception class for Panim.
 	'''
 	pass
+
+def is_closed(points):
+    return np.linalg.norm(points[0] - points[-1]) < CLOSED_THRESHOLD
